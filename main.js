5818e0aeaffba48f39a0caeb01432ae475fb1a96
hainz9658 <hainz9658@gmail.com>
2026-02-20 01:53:34 +0000

    update

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Add JS here
+
+const generatorBtn = document.getElementById('generator-btn');
+const numbersContainer = document.getElementById('numbers-container');
+
+generatorBtn.addEventListener('click', () => {
+  numbersContainer.innerHTML = '';
+  const lottoNumbers = new Set();
+  while (lottoNumbers.size < 6) {
+    lottoNumbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+  
+  const sortedNumbers = Array.from(lottoNumbers).sort((a, b) => a - b);
+
+  for (const number of sortedNumbers) {
+    const circle = document.createElement('div');
+    circle.classList.add('number-circle');
+    circle.textContent = number;
+    numbersContainer.appendChild(circle);
+  }
+});
